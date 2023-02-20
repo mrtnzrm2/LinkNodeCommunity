01d3e0e32d4385c54ee8d60e62a0365e328757e3
Jorge Martinez <csg1c12@gmail.com>
2023-02-20 15:11:19 -0500

    New specifications to toy.py

--- networks/toy.py
+++ networks/toy.py
@@ -9,8 +9,8 @@ class TOY(BASE):
   """
     Class to process the network in the format needed for the Hierarchy class to work.
     Hierarchy was created to use specifically in the macaque FLN network.
-    In the future, there will be another class to run the link community algorithm
-    in particular netowrks. For the moment, the network has to be directed.
+    **In the future, there will be another class to run the link community algorithm
+    in particular netowrks.** For the moment, the network has to be directed.
     
     Parameters:
         A: An NxM directed adjacency matrix.
@@ -22,7 +22,41 @@ class TOY(BASE):
           Currenly, only working with the single and average linkage method.
 
         nlog10: bool.
-          
+          If true, the data has to be transformed using a logarithmic transformation.
+          It also adds a "_l10" suffix to the plot directory.
+
+        lookup: bool.
+          If true, the zeros in the network will be assigned to a value coming from
+          the applied transformation of the data. Ex, in case of choosing the R1, or
+          inverse logarithmic trasnformation, the zeros in the network will be assigned
+          the maximum value of the new range. This modification will only contribute for
+          computing the similarities. They do not modify the topology of the network.
+
+        cut: bool.
+          If true, it tells the link community algorithm if it can skip link community
+          merging steps where the distance between link communities is constant.
+          It skips those steps unitl the algorithm merges two communities with
+          a new distance. It helps to reduce the computing time of the algorithm without
+          getting results very different from full analysis.
+
+        mapping: string.
+          Type of mapping applied to the network. There are three types.
+              >> "trivial": No transformation
+              >> "R1" : Inverse logarithmic transformation, i.e., applying x <- -log10(x)
+              >> "R2" : Normal logarithmic transformation, i.e., applying x <-  log10(x) + b
+        
+        index: string.
+          Type of similarity index. Currently, there are three types:
+            >> "jacp" : Jaccard probability index
+            >> "cos" : Cosine similarity
+            >> "jacw" : Modified weighted Jaccard index
+
+        topology: string.
+          Type of node neighborhoods to consider which could be:
+            >> "MIX" : Source similarities for in-links and target
+                similarities for out-links.
+            >> "SOURCE : Source similarities to in- and out-links.
+            >> "TARGET" : Target similarities to in- and out-links.
   """
   def __init__(
     self, A, linkage, nlog10=False, lookup=False, cut=False,
